1ce5f2fb21f321806be6a7e4dd980b5a70bbef61
ykwimk <ykwimk@gmail.com>
2025-05-24 11:41:20 +0900

    feat: og-image 추가 및 필요없는 파일 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,16 +7,16 @@ export const metadata: Metadata = {
   description:
     '마케터들을 위한 가벼운 커뮤니티, 마실에서 함께 성장하고 배우며 인사이트를 나눠보세요.',
   keywords:
-    '마케팅 커뮤니티, 마케터 모임, 마케팅 인사이트, 마케팅 네트워킹, 마실',
+    '마케팅, 마케팅 커뮤니티, 마케터 모임, 마케팅 인사이트, 마케팅 네트워킹, 마실',
   openGraph: {
     title: '마실 | 마케터의 가벼운 마실길',
     description:
       '마케터들을 위한 가벼운 커뮤니티, 마실에서 함께 성장하고 배우며 인사이트를 나눠보세요.',
-    url: 'https://masil.community',
+    url: 'https://masil-web.vercel.app',
     siteName: '마실',
     images: [
       {
-        url: '/og-image.jpg',
+        url: '/og-image.png',
         width: 1200,
         height: 630,
         alt: '마실 - 마케터의 가벼운 마실길',
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     title: '마실 | 마케터의 가벼운 마실길',
     description:
       '마케터들을 위한 가벼운 커뮤니티, 마실에서 함께 성장하고 배우며 인사이트를 나눠보세요.',
-    images: ['/og-image.jpg'],
+    images: ['/og-image.png'],
   },
 };
 
