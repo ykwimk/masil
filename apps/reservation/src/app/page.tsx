a6789c02ad183572d511fdafba9d1484dbcb8bea
ykwimk <ykwimk@gmail.com>
2025-09-29 12:11:21 +0900

    refactor: 필요없는 파일 및 코드 제거

--- apps/reservation/src/app/page.tsx
+++ apps/reservation/src/app/page.tsx
@@ -1,22 +1,12 @@
-// import AboutSection from '@/components/AboutSection';
-import FeaturesSection from '@/components/FeaturesSection';
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import HeroSection from '@/components/HeroSection';
-import JoinSection from '@/components/JoinSection';
-import PartnersSection from '@/components/PartnersSection';
-import TestimonialsSection from '@/components/TestimonialSection';
 
 export default function HomePage() {
   return (
     <main className="flex min-h-dvh flex-col overflow-hidden">
       <Header />
       <HeroSection />
-      {/* <AboutSection /> */}
-      <FeaturesSection />
-      <TestimonialsSection />
-      <PartnersSection />
-      <JoinSection />
       <Footer />
     </main>
   );
