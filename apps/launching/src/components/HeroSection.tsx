cef20e37a94dc1001bc43cda189a7214eea2fce9
ykwimk <ykwimk@gmail.com>
2025-09-30 09:15:02 +0900

    chore: apps/reservation > apps/launching 네이밍 변경

--- apps/launching/src/components/HeroSection.tsx
+++ apps/launching/src/components/HeroSection.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { motion } from 'motion/react';
-import { Button } from '../components/ui/button';
+import { Button } from './ui/button';
 
 export default function HeroSection() {
   return (
